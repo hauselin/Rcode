bde7f775dc43d6d23f993ae65f36c2c270f605b1
Hause Lin <hauselin@gmail.com>
2017-06-30 02:14:31 -0400

    v1

---
+++ reportMLM.R
@@ -0,0 +1,97 @@
+reportMLM <- function(model, decimal = 3, intercept = FALSE, format = 'apa', partR2 = FALSE) {
+    
+    # APA format
+    message("Fixed effects for MLM with effect sizes r, d, and semi-partial R (only works with lme and lmer fitted models)")
+    cat("r: .10 (small), .30 (medium), .50 (large) (Cohen, 1992)\nd: 0.20 (small), 0.50 (medium), .80 (large) (Cohen, 1992)\nR2: .02 (small), .13 (medium), .26 (large) (Cohen, 1992)")
+    
+    estimates <- data.frame(coef(summary(model)))
+    effectNames <- rownames(estimates)
+    colnames(estimates) <- tolower(colnames(estimates))
+    colnames(estimates) <- c('estimate', 'se', 'df', 'statistic', 'p')
+    
+    # effect sizes (Kashdan & Steger, 2006)
+    estimates$es.r <-  sqrt((estimates$statistic ^ 2 / (estimates$statistic ^ 2 + estimates$df))) # r
+    estimates$es.d <-  (2 * estimates$statistic) / sqrt(estimates$df) # d
+    
+    # if partR2 == TRUE, compute partial R square
+    if (partR2) {
+        # effect size semi partial R (Edwards et al., 2008)
+        anovaModel <- anova(model)
+        Fs <- anovaModel$F # F-values for each effect (marginal = type 3 SS with Satterthwaite (requires lmerTest package))
+        numDF <- anovaModel$NumDF #numerator DFs
+        denDF <- anovaModel$DenDF #denominator DFs
+        semiPartialREffect <- (numDF / denDF * Fs) / (1 + (numDF / denDF * Fs)) # effect sizes
+        estimates$es.partR2 <- c(NA, semiPartialREffect)   
+    }
+    
+    # print formatted results
+    if (intercept) {
+        startingRow <- 1 # if intercept == TRUE, then report intercept too
+    } else {
+        startingRow <- 2 # if intercept == FALSE, then don't report intercept
+    }
+    
+    for (i in startingRow:nrow(estimates)) { # for each fixed effect, format output accordingly (print line by line)
+        
+        # i <- i
+        estimateV <- estimates[i, 'estimate']
+        estimateV <- ifelse(abs(estimateV) < 0.01, round(estimateV, 3), round(estimateV, 2))
+        estimateSign <- sign(estimateV) # sign (negative or positive)
+        estimateV <- ifelse(abs(estimateV) < 0.01, 
+                             sprintf('%.3f', abs(estimateV)), 
+                             sprintf('%.2f', abs(estimateV))) # character (absolute value)
+        estimateV <- ifelse(estimateSign == -1, paste0("–", estimateV), estimateV) # if negative, add minus sign to character vector
+        
+        seV <- estimates[i, 'se']
+        seV <- ifelse(abs(seV) < 0.01, round(seV, 3), round(seV, 2))
+        seV <- ifelse(abs(seV) < 0.01, 
+                      sprintf('%.3f', abs(seV)), 
+                      sprintf('%.2f', abs(seV))) # character
+        dfV <- round(estimates[i, 'df'])
+        statisticV <- estimates[i, 'statistic']
+        statisticSign <- sign(statisticV)
+        statisticV <- ifelse(abs(statisticV) < 0.01, 
+                             sprintf('%.3f', abs(statisticV)), 
+                             sprintf('%.2f', abs(statisticV))) # character
+        statisticV <- ifelse(statisticSign == -1, paste0("–", statisticV), statisticV) # character
+        
+        pV <- estimates[i, 'p']
+        rV <- estimates[i, 'es.r']
+        
+        if (pV >= 0.001) { # if p value is >= .001, always print exact p value
+            
+            if (format == 'nn') { # if nature neuro format
+                
+                message(sprintf("%.0f: b = %s, SE = %s, t%.0f = %s, P = %.3f, r = %.2f",
+                                i, estimateV, seV, dfV, statisticV, pV, rV))  
+                
+            } else { # else always return APA format
+            
+                message(sprintf("%.0f: b = %s, SE = %s, t(%.0f) = %s, p = %s, r = %s",
+                                i, estimateV, seV, dfV, statisticV, substring(sprintf('%.3f', pV), 2), substring(sprintf('%.2f', rV), 2)))
+
+            }
+            
+        } else if (pV < 0.001) { # if p value < .001, then always print p < .001
+            
+            pV <- 0.001
+            
+            if (format == 'nn') {
+                
+                message(sprintf("%.0f: b = %s, SE = %s, t%.0f = %s, P < %.3f, r = %.2f",
+                                i, estimateV, seV, dfV, statisticV, pV, rV))  
+
+            } else {
+                
+                message(sprintf("%.0f: b = %s, SE = %s, t(%.0f) = %s, p < %s, r = %s",
+                                i, estimateV, seV, dfV, statisticV, substring(sprintf('%.3f', pV), 2), substring(sprintf('%.2f', rV), 2)))
+                
+            }
+            
+        }
+    }
+    
+    rownames(estimates) <- paste(1:nrow(estimates), rownames(estimates)) # add row number to summary table
+    
+    return(round(estimates, decimal))
+}
