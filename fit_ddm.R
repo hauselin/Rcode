ace88f484afbd01b31639f3247b8623821823486
Hause Lin <hauselin@gmail.com>
2018-06-22 22:27:50 -0400

    library tidyverse

--- fit_ddm.R
+++ fit_ddm.R
@@ -1,9 +1,9 @@
 # install packages
-packages <- c("rtdists", "ucminf", "data.table", "dplyr")
+packages <- c("rtdists", "ucminf", "data.table", "tidyverse")
 toInstall <- packages[!(packages %in% installed.packages()[,"Package"])]
 if (length(toInstall)) install.packages(toInstall)
 rm(packages); rm(toInstall) 
-library(rtdists); library(ucminf); library(data.table); library(dplyr)
+library(rtdists); library(ucminf); library(data.table); library(tidyverse)
 
 fit_ddm <- function(data, rts, responses, id = NULL, group = NULL, startParams = c(a = 2, v = 0.1, t0 = 0.3, z = 0.5)) {
     
