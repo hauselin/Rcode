a7813ad0d7ac0b3ac2490e7369360e911c6cc45a
Hause Lin <hauselin@gmail.com>
2019-08-05 22:38:35 -0400

    johnny function

---
+++ shorts_in_public.R
@@ -0,0 +1,28 @@
+shorts_in_public <- function() {
+    question <- "Should I wear shorts in public? Designed and made for Johnny Dubois."
+    message(question)
+    cat("Respond yes (y) or no (n) to the following questions to figure out.")
+
+    r1 <- readline("Are you under 12? ")
+    if (r1 == "y") {
+        message("Have fun and wear your shorts, little man.")
+    } else {
+        r2 <- readline("Are you at the beach? ")
+        if (r2 == "y") {
+            message("Sure, go ahead and wear your shorts and enjoy your swim...")
+        } else {
+            r3 <- readline("Are you playing sports? ")
+            if (r3 == "y") {
+                message("Yea, OK, since you insist. Put on your shorts...")
+            } else {
+                r4 <- readline("Are you in North Africa helping the British defeat Erwin Rommel? ")
+                if (r4 == "y") {
+                    cat("Fine, weirdo... Rock your shorts.")
+                } else {
+                    warning("PUT SOME DAMN PANTS ON, JOHNNY!")
+                }
+            }
+        }
+    }
+
+}
